a0a7fac22fd9a1cc7568757697f1469db1134230
Mockingbird <hide0mockingbird@gmail.com>
2020-12-10 19:40:39 +0700

    Schema Yup

--- src/Component/LoginForm.tsx
+++ src/Component/LoginForm.tsx
@@ -1,11 +1,21 @@
 import React, { FC } from "react";
+import * as yup from "yup";
 import { Formik, Form, Field } from "formik";
 import { MyField } from "../Hook/MyField";
+
+const Schema = yup.object().shape({
+  Email: yup.string().email("Invalid Email").required("This field is required"),
+  Password: yup
+    .string()
+    .min(6, "Please Enter less then 6 letters")
+    .required("This field required"),
+});
 
 export const LoginForm: FC = () => {
   return (
     <div>
       <Formik
+        validationSchema={Schema}
         initialValues={{ Email: "", Password: "", RememberMe: false }}
         onSubmit={(data, { setSubmitting }) => {
           setSubmitting(true);
@@ -13,14 +23,14 @@ export const LoginForm: FC = () => {
           setSubmitting(false);
         }}
       >
-        {({ values, isSubmitting }) => (
+        {({ values, errors, isSubmitting }) => (
           <Form>
             <div>
-              <label htmlFor="Email">Email</label>
+              <label htmlFor="email">Email</label>
               <MyField placeholder="Email" name="Email" type="Email" />
             </div>
             <div>
-              <label htmlFor="Password">Password</label>
+              <label htmlFor="password">Password</label>
               <MyField placeholder="Password" name="Password" type="Password" />
             </div>
             <div>
@@ -36,6 +46,7 @@ export const LoginForm: FC = () => {
             </div>
             <div>
               <pre>{JSON.stringify(values, null, 2)}</pre>
+              <pre>{JSON.stringify(errors, null, 2)}</pre>
             </div>
           </Form>
         )}
--- src/Hook/MyField.tsx
+++ src/Hook/MyField.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React from "react";
 import { Field, useField } from "formik";
 
 export const MyField = (props: any) => {
